3e34b9c054e3c8bf7d2b809c2365029b99ed67e9
Linat75 <linatamrin20@gmail.com>
2026-02-20 19:46:50 +0600

    Add files via upload

--- editor_plugin_src.js
+++ editor_plugin_src.js
@@ -6,45 +6,118 @@
  *
  * License: http://tinymce.moxiecode.com/license
  * Contributing: http://tinymce.moxiecode.com/contributing
+ *
+ * This plugin will force TinyMCE to produce deprecated legacy output such as font elements, u elements, align
+ * attributes and so forth. There are a few cases where these old items might be needed for example in email applications or with Flash
+ *
+ * However you should NOT use this plugin if you are building some system that produces web contents such as a CMS. All these elements are
+ * not apart of the newer specifications for HTML and XHTML.
  */
 
-(function() {
-	var each = tinymce.each;
+(function(tinymce) {
+	// Override inline_styles setting to force TinyMCE to produce deprecated contents
+	tinymce.onAddEditor.addToTop(function(tinymce, editor) {
+		editor.settings.inline_styles = false;
+	});
 
-	tinymce.create('tinymce.plugins.TemplatePlugin', {
-		init : function(ed, url) {
-			var t = this;
+	// Create the legacy ouput plugin
+	tinymce.create('tinymce.plugins.LegacyOutput', {
+		init : function(editor) {
+			editor.onInit.add(function() {
+				var alignElements = 'p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img',
+					fontSizes = tinymce.explode(editor.settings.font_size_style_values),
+					schema = editor.schema;
 
-			t.editor = ed;
+				// Override some internal formats to produce legacy elements and attributes
+				editor.formatter.register({
+					// Change alignment formats to use the deprecated align attribute
+					alignleft : {selector : alignElements, attributes : {align : 'left'}},
+					aligncenter : {selector : alignElements, attributes : {align : 'center'}},
+					alignright : {selector : alignElements, attributes : {align : 'right'}},
+					alignfull : {selector : alignElements, attributes : {align : 'justify'}},
 
-			// Register commands
-			ed.addCommand('mceTemplate', function(ui) {
-				ed.windowManager.open({
-					file : url + '/template.htm',
-					width : ed.getParam('template_popup_width', 750),
-					height : ed.getParam('template_popup_height', 600),
-					inline : 1
-				}, {
-					plugin_url : url
+					// Change the basic formatting elements to use deprecated element types
+					bold : [
+						{inline : 'b', remove : 'all'},
+						{inline : 'strong', remove : 'all'},
+						{inline : 'span', styles : {fontWeight : 'bold'}}
+					],
+					italic : [
+						{inline : 'i', remove : 'all'},
+						{inline : 'em', remove : 'all'},
+						{inline : 'span', styles : {fontStyle : 'italic'}}
+					],
+					underline : [
+						{inline : 'u', remove : 'all'},
+						{inline : 'span', styles : {textDecoration : 'underline'}, exact : true}
+					],
+					strikethrough : [
+						{inline : 'strike', remove : 'all'},
+						{inline : 'span', styles : {textDecoration: 'line-through'}, exact : true}
+					],
+
+					// Change font size and font family to use the deprecated font element
+					fontname : {inline : 'font', attributes : {face : '%value'}},
+					fontsize : {
+						inline : 'font',
+						attributes : {
+							size : function(vars) {
+								return tinymce.inArray(fontSizes, vars.value) + 1;
+							}
+						}
+					},
+
+					// Setup font elements for colors as well
+					forecolor : {inline : 'font', styles : {color : '%value'}},
+					hilitecolor : {inline : 'font', styles : {backgroundColor : '%value'}}
 				});
-			});
 
-			ed.addCommand('mceInsertTemplate', t._insertTemplate, t);
+				// Check that deprecated elements are allowed if not add them
+				tinymce.each('b,i,u,strike'.split(','), function(name) {
+					schema.addValidElements(name + '[*]');
+				});
 
-			// Register buttons
-			ed.addButton('template', {title : 'template.desc', cmd : 'mceTemplate'});
+				// Add font element if it's missing
+				if (!schema.getElementRule("font"))
+					schema.addValidElements("font[face|size|color|style]");
 
-			ed.onPreProcess.add(function(ed, o) {
-				var dom = ed.dom;
+				// Add the missing and depreacted align attribute for the serialization engine
+				tinymce.each(alignElements.split(','), function(name) {
+					var rule = schema.getElementRule(name), found;
 
-				each(dom.select('div', o.node), function(e) {
-					if (dom.hasClass(e, 'mceTmpl')) {
-						each(dom.select('*', e), function(e) {
-							if (dom.hasClass(e, ed.getParam('template_mdate_classes', 'mdate').replace(/\s+/g, '|')))
-								e.innerHTML = t._getDateTime(new Date(), ed.getParam("template_mdate_format", ed.getLang("template.mdate_format")));
+					if (rule) {
+						if (!rule.attributes.align) {
+							rule.attributes.align = {};
+							rule.attributesOrder.push('align');
+						}
+					}
+				});
+
+				// Listen for the onNodeChange event so that we can do special logic for the font size and font name drop boxes
+				editor.onNodeChange.add(function(editor, control_manager) {
+					var control, fontElm, fontName, fontSize;
+
+					// Find font element get it's name and size
+					fontElm = editor.dom.getParent(editor.selection.getNode(), 'font');
+					if (fontElm) {
+						fontName = fontElm.face;
+						fontSize = fontElm.size;
+					}
+
+					// Select/unselect the font name in droplist
+					if (control = control_manager.get('fontselect')) {
+						control.select(function(value) {
+							return value == fontName;
 						});
+					}
 
-						t._replaceVals(e);
+					// Select/unselect the font size in droplist
+					if (control = control_manager.get('fontsizeselect')) {
+						control.select(function(value) {
+							var index = tinymce.inArray(fontSizes, value.fontSize);
+
+							return index + 1 == fontSize;
+						});
 					}
 				});
 			});
@@ -52,108 +125,15 @@
 
 		getInfo : function() {
 			return {
-				longname : 'Template plugin',
+				longname : 'LegacyOutput',
 				author : 'Moxiecode Systems AB',
-				authorurl : 'http://www.moxiecode.com',
-				infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/template',
+				authorurl : 'http://tinymce.moxiecode.com',
+				infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/legacyoutput',
 				version : tinymce.majorVersion + "." + tinymce.minorVersion
 			};
-		},
-
-		_insertTemplate : function(ui, v) {
-			var t = this, ed = t.editor, h, el, dom = ed.dom, sel = ed.selection.getContent();
-
-			h = v.content;
-
-			each(t.editor.getParam('template_replace_values'), function(v, k) {
-				if (typeof(v) != 'function')
-					h = h.replace(new RegExp('\\{\\$' + k + '\\}', 'g'), v);
-			});
-
-			el = dom.create('div', null, h);
-
-			// Find template element within div
-			n = dom.select('.mceTmpl', el);
-			if (n && n.length > 0) {
-				el = dom.create('div', null);
-				el.appendChild(n[0].cloneNode(true));
-			}
-
-			function hasClass(n, c) {
-				return new RegExp('\\b' + c + '\\b', 'g').test(n.className);
-			};
-
-			each(dom.select('*', el), function(n) {
-				// Replace cdate
-				if (hasClass(n, ed.getParam('template_cdate_classes', 'cdate').replace(/\s+/g, '|')))
-					n.innerHTML = t._getDateTime(new Date(), ed.getParam("template_cdate_format", ed.getLang("template.cdate_format")));
-
-				// Replace mdate
-				if (hasClass(n, ed.getParam('template_mdate_classes', 'mdate').replace(/\s+/g, '|')))
-					n.innerHTML = t._getDateTime(new Date(), ed.getParam("template_mdate_format", ed.getLang("template.mdate_format")));
-
-				// Replace selection
-				if (hasClass(n, ed.getParam('template_selected_content_classes', 'selcontent').replace(/\s+/g, '|')))
-					n.innerHTML = sel;
-			});
-
-			t._replaceVals(el);
-
-			ed.execCommand('mceInsertContent', false, el.innerHTML);
-			ed.addVisual();
-		},
-
-		_replaceVals : function(e) {
-			var dom = this.editor.dom, vl = this.editor.getParam('template_replace_values');
-
-			each(dom.select('*', e), function(e) {
-				each(vl, function(v, k) {
-					if (dom.hasClass(e, k)) {
-						if (typeof(vl[k]) == 'function')
-							vl[k](e);
-					}
-				});
-			});
-		},
-
-		_getDateTime : function(d, fmt) {
-				if (!fmt)
-					return "";
-
-				function addZeros(value, len) {
-					var i;
-
-					value = "" + value;
-
-					if (value.length < len) {
-						for (i=0; i<(len-value.length); i++)
-							value = "0" + value;
-					}
-
-					return value;
-				}
-
-				fmt = fmt.replace("%D", "%m/%d/%y");
-				fmt = fmt.replace("%r", "%I:%M:%S %p");
-				fmt = fmt.replace("%Y", "" + d.getFullYear());
-				fmt = fmt.replace("%y", "" + d.getYear());
-				fmt = fmt.replace("%m", addZeros(d.getMonth()+1, 2));
-				fmt = fmt.replace("%d", addZeros(d.getDate(), 2));
-				fmt = fmt.replace("%H", "" + addZeros(d.getHours(), 2));
-				fmt = fmt.replace("%M", "" + addZeros(d.getMinutes(), 2));
-				fmt = fmt.replace("%S", "" + addZeros(d.getSeconds(), 2));
-				fmt = fmt.replace("%I", "" + ((d.getHours() + 11) % 12 + 1));
-				fmt = fmt.replace("%p", "" + (d.getHours() < 12 ? "AM" : "PM"));
-				fmt = fmt.replace("%B", "" + this.editor.getLang("template_months_long").split(',')[d.getMonth()]);
-				fmt = fmt.replace("%b", "" + this.editor.getLang("template_months_short").split(',')[d.getMonth()]);
-				fmt = fmt.replace("%A", "" + this.editor.getLang("template_day_long").split(',')[d.getDay()]);
-				fmt = fmt.replace("%a", "" + this.editor.getLang("template_day_short").split(',')[d.getDay()]);
-				fmt = fmt.replace("%%", "%");
-
-				return fmt;
 		}
 	});
 
 	// Register plugin
-	tinymce.PluginManager.add('template', tinymce.plugins.TemplatePlugin);
-})();
+	tinymce.PluginManager.add('legacyoutput', tinymce.plugins.LegacyOutput);
+})(tinymce);
