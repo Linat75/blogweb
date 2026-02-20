0d3bb160fec6b3526a3ff432a51e92b0427ccc53
Linat75 <linatamrin20@gmail.com>
2026-02-20 19:05:31 +0600

    Add files via upload

--- editor_plugin_src.js
+++ editor_plugin_src.js
@@ -9,18 +9,22 @@
  */
 
 (function() {
-	tinymce.create('tinymce.plugins.AdvancedImagePlugin', {
+	tinymce.create('tinymce.plugins.AdvancedLinkPlugin', {
 		init : function(ed, url) {
+			this.editor = ed;
+
 			// Register commands
-			ed.addCommand('mceAdvImage', function() {
-				// Internal image object like a flash placeholder
-				if (ed.dom.getAttrib(ed.selection.getNode(), 'class', '').indexOf('mceItem') != -1)
+			ed.addCommand('mceAdvLink', function() {
+				var se = ed.selection;
+
+				// No selection and not in link
+				if (se.isCollapsed() && !ed.dom.getParent(se.getNode(), 'A'))
 					return;
 
 				ed.windowManager.open({
-					file : url + '/image.htm',
-					width : 480 + parseInt(ed.getLang('advimage.delta_width', 0)),
-					height : 385 + parseInt(ed.getLang('advimage.delta_height', 0)),
+					file : url + '/link.htm',
+					width : 480 + parseInt(ed.getLang('advlink.delta_width', 0)),
+					height : 400 + parseInt(ed.getLang('advlink.delta_height', 0)),
 					inline : 1
 				}, {
 					plugin_url : url
@@ -28,23 +32,30 @@
 			});
 
 			// Register buttons
-			ed.addButton('image', {
-				title : 'advimage.image_desc',
-				cmd : 'mceAdvImage'
+			ed.addButton('link', {
+				title : 'advlink.link_desc',
+				cmd : 'mceAdvLink'
+			});
+
+			ed.addShortcut('ctrl+k', 'advlink.advlink_desc', 'mceAdvLink');
+
+			ed.onNodeChange.add(function(ed, cm, n, co) {
+				cm.setDisabled('link', co && n.nodeName != 'A');
+				cm.setActive('link', n.nodeName == 'A' && !n.name);
 			});
 		},
 
 		getInfo : function() {
 			return {
-				longname : 'Advanced image',
+				longname : 'Advanced link',
 				author : 'Moxiecode Systems AB',
 				authorurl : 'http://tinymce.moxiecode.com',
-				infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/advimage',
+				infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/advlink',
 				version : tinymce.majorVersion + "." + tinymce.minorVersion
 			};
 		}
 	});
 
 	// Register plugin
-	tinymce.PluginManager.add('advimage', tinymce.plugins.AdvancedImagePlugin);
+	tinymce.PluginManager.add('advlink', tinymce.plugins.AdvancedLinkPlugin);
 })();
