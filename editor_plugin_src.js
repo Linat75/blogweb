acccec29dfe9b8fdfab020bdf92e97f9e07c71fb
Linat75 <linatamrin20@gmail.com>
2026-02-20 19:10:00 +0600

    Add files via upload

--- editor_plugin_src.js
+++ editor_plugin_src.js
@@ -9,124 +9,112 @@
  */
 
 (function() {
-	tinymce.create('tinymce.plugins.XHTMLXtrasPlugin', {
+	tinymce.create('tinymce.plugins.BBCodePlugin', {
 		init : function(ed, url) {
-			// Register commands
-			ed.addCommand('mceCite', function() {
-				ed.windowManager.open({
-					file : url + '/cite.htm',
-					width : 350 + parseInt(ed.getLang('xhtmlxtras.cite_delta_width', 0)),
-					height : 250 + parseInt(ed.getLang('xhtmlxtras.cite_delta_height', 0)),
-					inline : 1
-				}, {
-					plugin_url : url
-				});
+			var t = this, dialect = ed.getParam('bbcode_dialect', 'punbb').toLowerCase();
+
+			ed.onBeforeSetContent.add(function(ed, o) {
+				o.content = t['_' + dialect + '_bbcode2html'](o.content);
 			});
 
-			ed.addCommand('mceAcronym', function() {
-				ed.windowManager.open({
-					file : url + '/acronym.htm',
-					width : 350 + parseInt(ed.getLang('xhtmlxtras.acronym_delta_width', 0)),
-					height : 250 + parseInt(ed.getLang('xhtmlxtras.acronym_delta_height', 0)),
-					inline : 1
-				}, {
-					plugin_url : url
-				});
-			});
+			ed.onPostProcess.add(function(ed, o) {
+				if (o.set)
+					o.content = t['_' + dialect + '_bbcode2html'](o.content);
 
-			ed.addCommand('mceAbbr', function() {
-				ed.windowManager.open({
-					file : url + '/abbr.htm',
-					width : 350 + parseInt(ed.getLang('xhtmlxtras.abbr_delta_width', 0)),
-					height : 250 + parseInt(ed.getLang('xhtmlxtras.abbr_delta_height', 0)),
-					inline : 1
-				}, {
-					plugin_url : url
-				});
-			});
-
-			ed.addCommand('mceDel', function() {
-				ed.windowManager.open({
-					file : url + '/del.htm',
-					width : 340 + parseInt(ed.getLang('xhtmlxtras.del_delta_width', 0)),
-					height : 310 + parseInt(ed.getLang('xhtmlxtras.del_delta_height', 0)),
-					inline : 1
-				}, {
-					plugin_url : url
-				});
-			});
-
-			ed.addCommand('mceIns', function() {
-				ed.windowManager.open({
-					file : url + '/ins.htm',
-					width : 340 + parseInt(ed.getLang('xhtmlxtras.ins_delta_width', 0)),
-					height : 310 + parseInt(ed.getLang('xhtmlxtras.ins_delta_height', 0)),
-					inline : 1
-				}, {
-					plugin_url : url
-				});
-			});
-
-			ed.addCommand('mceAttributes', function() {
-				ed.windowManager.open({
-					file : url + '/attributes.htm',
-					width : 380 + parseInt(ed.getLang('xhtmlxtras.attr_delta_width', 0)),
-					height : 370 + parseInt(ed.getLang('xhtmlxtras.attr_delta_height', 0)),
-					inline : 1
-				}, {
-					plugin_url : url
-				});
-			});
-
-			// Register buttons
-			ed.addButton('cite', {title : 'xhtmlxtras.cite_desc', cmd : 'mceCite'});
-			ed.addButton('acronym', {title : 'xhtmlxtras.acronym_desc', cmd : 'mceAcronym'});
-			ed.addButton('abbr', {title : 'xhtmlxtras.abbr_desc', cmd : 'mceAbbr'});
-			ed.addButton('del', {title : 'xhtmlxtras.del_desc', cmd : 'mceDel'});
-			ed.addButton('ins', {title : 'xhtmlxtras.ins_desc', cmd : 'mceIns'});
-			ed.addButton('attribs', {title : 'xhtmlxtras.attribs_desc', cmd : 'mceAttributes'});
-
-			ed.onNodeChange.add(function(ed, cm, n, co) {
-				n = ed.dom.getParent(n, 'CITE,ACRONYM,ABBR,DEL,INS');
-
-				cm.setDisabled('cite', co);
-				cm.setDisabled('acronym', co);
-				cm.setDisabled('abbr', co);
-				cm.setDisabled('del', co);
-				cm.setDisabled('ins', co);
-				cm.setDisabled('attribs', n && n.nodeName == 'BODY');
-				cm.setActive('cite', 0);
-				cm.setActive('acronym', 0);
-				cm.setActive('abbr', 0);
-				cm.setActive('del', 0);
-				cm.setActive('ins', 0);
-
-				// Activate all
-				if (n) {
-					do {
-						cm.setDisabled(n.nodeName.toLowerCase(), 0);
-						cm.setActive(n.nodeName.toLowerCase(), 1);
-					} while (n = n.parentNode);
-				}
-			});
-
-			ed.onPreInit.add(function() {
-				// Fixed IE issue where it can't handle these elements correctly
-				ed.dom.create('abbr');
+				if (o.get)
+					o.content = t['_' + dialect + '_html2bbcode'](o.content);
 			});
 		},
 
 		getInfo : function() {
 			return {
-				longname : 'XHTML Xtras Plugin',
+				longname : 'BBCode Plugin',
 				author : 'Moxiecode Systems AB',
 				authorurl : 'http://tinymce.moxiecode.com',
-				infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/xhtmlxtras',
+				infourl : 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/bbcode',
 				version : tinymce.majorVersion + "." + tinymce.minorVersion
 			};
+		},
+
+		// Private methods
+
+		// HTML -> BBCode in PunBB dialect
+		_punbb_html2bbcode : function(s) {
+			s = tinymce.trim(s);
+
+			function rep(re, str) {
+				s = s.replace(re, str);
+			};
+
+			// example: <strong> to [b]
+			rep(/<a.*?href=\"(.*?)\".*?>(.*?)<\/a>/gi,"[url=$1]$2[/url]");
+			rep(/<font.*?color=\"(.*?)\".*?class=\"codeStyle\".*?>(.*?)<\/font>/gi,"[code][color=$1]$2[/color][/code]");
+			rep(/<font.*?color=\"(.*?)\".*?class=\"quoteStyle\".*?>(.*?)<\/font>/gi,"[quote][color=$1]$2[/color][/quote]");
+			rep(/<font.*?class=\"codeStyle\".*?color=\"(.*?)\".*?>(.*?)<\/font>/gi,"[code][color=$1]$2[/color][/code]");
+			rep(/<font.*?class=\"quoteStyle\".*?color=\"(.*?)\".*?>(.*?)<\/font>/gi,"[quote][color=$1]$2[/color][/quote]");
+			rep(/<span style=\"color: ?(.*?);\">(.*?)<\/span>/gi,"[color=$1]$2[/color]");
+			rep(/<font.*?color=\"(.*?)\".*?>(.*?)<\/font>/gi,"[color=$1]$2[/color]");
+			rep(/<span style=\"font-size:(.*?);\">(.*?)<\/span>/gi,"[size=$1]$2[/size]");
+			rep(/<font>(.*?)<\/font>/gi,"$1");
+			rep(/<img.*?src=\"(.*?)\".*?\/>/gi,"[img]$1[/img]");
+			rep(/<span class=\"codeStyle\">(.*?)<\/span>/gi,"[code]$1[/code]");
+			rep(/<span class=\"quoteStyle\">(.*?)<\/span>/gi,"[quote]$1[/quote]");
+			rep(/<strong class=\"codeStyle\">(.*?)<\/strong>/gi,"[code][b]$1[/b][/code]");
+			rep(/<strong class=\"quoteStyle\">(.*?)<\/strong>/gi,"[quote][b]$1[/b][/quote]");
+			rep(/<em class=\"codeStyle\">(.*?)<\/em>/gi,"[code][i]$1[/i][/code]");
+			rep(/<em class=\"quoteStyle\">(.*?)<\/em>/gi,"[quote][i]$1[/i][/quote]");
+			rep(/<u class=\"codeStyle\">(.*?)<\/u>/gi,"[code][u]$1[/u][/code]");
+			rep(/<u class=\"quoteStyle\">(.*?)<\/u>/gi,"[quote][u]$1[/u][/quote]");
+			rep(/<\/(strong|b)>/gi,"[/b]");
+			rep(/<(strong|b)>/gi,"[b]");
+			rep(/<\/(em|i)>/gi,"[/i]");
+			rep(/<(em|i)>/gi,"[i]");
+			rep(/<\/u>/gi,"[/u]");
+			rep(/<span style=\"text-decoration: ?underline;\">(.*?)<\/span>/gi,"[u]$1[/u]");
+			rep(/<u>/gi,"[u]");
+			rep(/<blockquote[^>]*>/gi,"[quote]");
+			rep(/<\/blockquote>/gi,"[/quote]");
+			rep(/<br \/>/gi,"\n");
+			rep(/<br\/>/gi,"\n");
+			rep(/<br>/gi,"\n");
+			rep(/<p>/gi,"");
+			rep(/<\/p>/gi,"\n");
+			rep(/&nbsp;|\u00a0/gi," ");
+			rep(/&quot;/gi,"\"");
+			rep(/&lt;/gi,"<");
+			rep(/&gt;/gi,">");
+			rep(/&amp;/gi,"&");
+
+			return s; 
+		},
+
+		// BBCode -> HTML from PunBB dialect
+		_punbb_bbcode2html : function(s) {
+			s = tinymce.trim(s);
+
+			function rep(re, str) {
+				s = s.replace(re, str);
+			};
+
+			// example: [b] to <strong>
+			rep(/\n/gi,"<br />");
+			rep(/\[b\]/gi,"<strong>");
+			rep(/\[\/b\]/gi,"</strong>");
+			rep(/\[i\]/gi,"<em>");
+			rep(/\[\/i\]/gi,"</em>");
+			rep(/\[u\]/gi,"<u>");
+			rep(/\[\/u\]/gi,"</u>");
+			rep(/\[url=([^\]]+)\](.*?)\[\/url\]/gi,"<a href=\"$1\">$2</a>");
+			rep(/\[url\](.*?)\[\/url\]/gi,"<a href=\"$1\">$1</a>");
+			rep(/\[img\](.*?)\[\/img\]/gi,"<img src=\"$1\" />");
+			rep(/\[color=(.*?)\](.*?)\[\/color\]/gi,"<font color=\"$1\">$2</font>");
+			rep(/\[code\](.*?)\[\/code\]/gi,"<span class=\"codeStyle\">$1</span>&nbsp;");
+			rep(/\[quote.*?\](.*?)\[\/quote\]/gi,"<span class=\"quoteStyle\">$1</span>&nbsp;");
+
+			return s; 
 		}
 	});
 
 	// Register plugin
-	tinymce.PluginManager.add('xhtmlxtras', tinymce.plugins.XHTMLXtrasPlugin);
+	tinymce.PluginManager.add('bbcode', tinymce.plugins.BBCodePlugin);
 })();
